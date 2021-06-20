8d59fcd5459c51eca5a7376ced23c2e7d71decd1
sfnvm <trungpt.dev@gmail.com>
2021-06-20 11:51:38 +0700

    add scripts, update vscode config

--- .binds.zsh
+++ .binds.zsh
@@ -1,2 +1,3 @@
 alias tecavpn='sudo openconnect -u t1-vq2-dev --os=win --passwd-on-stdin 125.212.205.1 --protocol=gp'
-#alias tecavpn='./sh/tecavpn.sh'
+alias tecavpnt1='sudo openconnect -u dungtv --os=win --passwd-on-stdin 103.237.60.251 --protocol=gp'
+alias tecavpnbhxh='sudo openconnect -u t1-tst-dev2 --os=win --passwd-on-stdin 125.212.205.1 --protocol=gp'
