07509878346f667e2b41227c63ec2816eb0e928f
sfnvm <trungpt.dev@gmail.com>
2021-06-21 17:48:43 +0700

    update

--- .binds.zsh
+++ .binds.zsh
@@ -1,3 +1,4 @@
 alias tecavpn='sudo openconnect -u t1-vq2-dev --os=win --passwd-on-stdin 125.212.205.1 --protocol=gp'
+alias tecavpn='sudo openconnect -u t1-dev2 --os=win --passwd-on-stdin 117.1.16.233 --protocol=gp'
 alias tecavpnt1='sudo openconnect -u dungtv --os=win --passwd-on-stdin 103.237.60.251 --protocol=gp'
 alias tecavpnbhxh='sudo openconnect -u t1-tst-dev2 --os=win --passwd-on-stdin 125.212.205.1 --protocol=gp'
